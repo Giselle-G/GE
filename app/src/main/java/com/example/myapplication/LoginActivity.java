eaae1d02799e583ee9e128ed56cdc23b1bcf3464
g <1183602250@qq.com>
2020-04-10 19:49:51 +0800

    test

---
+++ app/src/main/java/com/example/myapplication/LoginActivity.java
@@ -0,0 +1,17 @@
+package com.example.myapplication;
+
+import android.annotation.SuppressLint;
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+@SuppressLint("Registered")
+public class LoginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.home);
+
+    }
+}
